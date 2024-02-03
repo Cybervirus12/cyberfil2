33515793af12449fbeb7446422a2da44093ce2f9
Cybervirus12 <158598816+Cybervirus12@users.noreply.github.com>
2024-02-03 14:31:44 +0330

    Add files via upload

---
+++ cyberfil2(1).py
@@ -0,0 +1,37 @@
+from os import system
+from colorama import Fore
+from time import sleep
+system("clear")
+
+print("hello hackrs")
+print("")
+
+print(Fore.RED+"
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⠟⠛⠉⣩⣍⠉⠛⠻⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⠋⠀⠀⣠⣾⣿⠟⠁⠀⠀⠀⠙⣿⣿⣿⣿ ⣿⣿⣿⠁⠀⠀⢾⣿⣟⠁⠀⣠⣾⣷⣄⠀⠘⣿⣿⣿ ⣿⣿⡇⣠⣦⡀⠀⠙⢿⣷⣾⡿⠋⠻⣿⣷⣄⢸⣿⣿ ⣿⣿⡇⠙⢿⣿⣦⣠⣾⡿⢿⣷⣄⠀⠈⠻⠋⢸⣿⣿ ⣿⣿⣿⡀⠀⠙⢿⡿⠋⠀⢀⣽⣿⡷⠀⠀⢠⣿⣿⣿ ⣿⣿⣿⣿⣄⠀⠀⠀⢀⣴⣿⡿⠋⠀⠀⣠⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣦⣤⣀⣙⣋⣀⣤⣴⣿⣿⣿⣿⣿⣿ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿")
+
+code = input(Fore.MAGENTA+f"""
+(1) code fil ac
+(2) code fil grup
+(3) code fil chanel
+(4) mokhareb
+(5) id shad
+code>>>>..>""")
+print("")
+
+print(Fore.RED+"loding........")
+if code == "1":
+	print(Fore.GREEN+"9.1.7.7/f/h.g//8.4.1.6.1.6.3.5.3.3.7.8.2/f//.8.0.5.5.0.5.9.7.3.9.7.1.9.8.3.7.9.6.3.0.9.7.5.6/")
+
+elif code == "2":
+    print(Fore.GREEN+"3.1.0.0.0.7.2/f////g.h/2.9.3.7.9.9.4.8.2.2.4.1.8.9.2.5.2.9.4.0.7.8.7.7.0.8.6.4.0.1.4.3.1.4.2.2.7/")
+
+elif code == "3":
+	print(Fore.GREEN+"4.6.3.7/r//g/h8.0.3.8.5.5.5.0.4.4.5.5.3.1.8.4.7.7.7.1.8.2.6.4.7.2.3.4.0.2.6.2.3.9.4.2.6/")
+	
+elif code == "4":
+	print(Fore.RED+"https://bit.ly/3ild93L")
+	
+elif code == "5":
+	print("@NAZISM15818")
+else:
+	print(f"{Fore.RED}Please Enter A Number Between ( 1, 6 )")
